2cfc16f473a56217942a749901a81e6a30ba01c3
Kriti Sharda <kriti.sharda@retailbusinessservices.com>
2019-01-15 16:43:23 -0500

    basic setup for routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 
 import './App.css';
 import { TestComponent } from './components/TestComponent';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 class App extends Component {
   state = {
+    timestamp: '',
+    location: ''
 
   }
 
   render() {
     return (
-      <TestComponent/>
+      <div>
+        
+          <HashRouter>
+            <div>
+              <p>App JS Page</p>
+              <Route path="/test" component={TestComponent} />
+            </div>
+          </HashRouter>
+          {/* {alert("sadfgh")} */}
+      </div>
     );
   }
 }
