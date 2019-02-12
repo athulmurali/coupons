a46c947d0555c382d4feaca245839a4243d0430b
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-02-12 09:41:45 -0500

    Added SortFilterReducer

---
+++ src/redux/reducers/SortFilterReducer.js
@@ -0,0 +1,55 @@
+const initialState={
+    toBeFetched : false,
+    filters : [],
+    search:{},
+    arr: []
+}
+
+
+const MINIMUM_SEARCH_LENGTH = 3
+export const FETCH_COUPONS = "FETCH_COUPONS"
+
+const TestReducer =(state= initialState, action)=>{
+
+    switch(action.type)
+    {
+        case 'SET_FILTERS' : return  {
+            ...state,
+            filters : action.payload,
+            toBeFetched : true
+        }
+
+        case 'SET_SEARCH'  : return {
+            ...state,
+            search : action.payload.name,
+            toBeFetched : !!action.payload.name && action.payload.name.length >=MINIMUM_SEARCH_LENGTH
+        }
+
+        case FETCH_COUPONS + '_PENDING' : return {
+            ...state,
+            toBeFetched : false,
+            isLoading : false
+
+        }
+
+        case FETCH_COUPONS + '_REJECTED' : return {
+            ...state,
+            toBeFetched : false,
+            isLoading : false
+
+        }
+
+        case FETCH_COUPONS + '_FULFILLED' : {
+            return {
+                ...state,
+                toBeFetched : false,
+                isLoading : false,
+                arr : action.payload.data.results
+
+            }
+        }
+        default             : return {...state}
+    }
+}
+
+export  default  TestReducer
