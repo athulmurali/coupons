7e2de54f9c83ea528ec19545d78ef764fce687e8
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-02-14 21:58:19 -0500

    removed getLoadedCoupons function
    - The api for loaded coupons and all coupons is same, so there is no need for a seperate function any more and loaded coupon is obtained using the change of  boolean flag loaded

--- src/utils/services/test.js
+++ src/utils/services/test.js
@@ -4,12 +4,6 @@ import * as axios from 'axios'
 // https://github.com/axios/axios
 
 
-const baseURL =
-    "https://randomuser.me/api/"
-
-const axiosInstance =  axios.create({baseURL})
-
-
 const arrayToArrayString = (arrayObject )=>("["+ arrayObject.toString() +"]")
 const encodeSpecialChars=(strToEncode)=>(strToEncode.replace('&', '_'))
 
@@ -67,30 +61,5 @@ export const getAllCoupons =  (searchParams, filterParams, sortParams) => {
         params : {
             ...processedQueryParams
         }
-    })
-}
-
-
-// baseUrl to be updated
-export const getLoadedCoupons =  (searchParams, filterParams, sortParams) => {
-
-    console.log(searchParams)
-    const queryParams = {
-        ...searchParams,
-        ...filterParams,
-        ...sortParams
-    }
-
-    console.log(queryParams)
-    axiosInstance.get({
-        params: {...queryParams}
-    }).then(console.log)
-
-     const tempUrl = "http://innovationd.aholdusa.com:3526/couponServer/coupons/fetchCouponsByFilter/?sortBy=price&sortOrder=asc&filterByString=[Soups%20_%20Canned%20Goods]&searchString=Kel&loaded=false"
-   return  axiosInstance.get(tempUrl, {
-        // params: {
-        //     nat: 'us',
-        //     inc: 'name,picture,email,results=10&noinfo'
-        // }
     })
 }
