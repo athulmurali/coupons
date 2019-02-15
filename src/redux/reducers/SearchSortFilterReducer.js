f8a99f5cf8dd2163a3a92b0aa62ea858341d2866
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-02-14 23:41:13 -0500

    Fixed search Component
    - Fixed sortComponent inputs such that it can pass sortBy and sortOrder values
    - Added RESET action listener  SearchSortFilterReducer to clear the existing filters , sort and search once the  user logs in after log out

--- src/redux/reducers/SearchSortFilterReducer.js
+++ src/redux/reducers/SearchSortFilterReducer.js
@@ -1,84 +1,90 @@
-export const FETCH_COUPONS = "FETCH_COUPONS"
-export const SET_SORT = 'SET_SORT'
-export const SET_FILTERS = 'SET_FILTERS'
-export const SET_SEARCH = 'SET_SEARCH'
+import {RESET} from "./DisplayCouponReducer";
 
-export const FETCH_COUPONS_PENDING = FETCH_COUPONS + '_PENDING'
-export const FETCH_COUPONS_REJECTED = FETCH_COUPONS + '_REJECTED'
-export const FETCH_COUPONS_FULFILLED = FETCH_COUPONS + '_FULFILLED'
+export const FETCH_COUPONS = "FETCH_COUPONS";
+export const SET_SORT = "SET_SORT";
+export const SET_FILTERS = "SET_FILTERS";
+export const SET_SEARCH = "SET_SEARCH";
+
+export const FETCH_COUPONS_PENDING = FETCH_COUPONS + "_PENDING";
+export const FETCH_COUPONS_REJECTED = FETCH_COUPONS + "_REJECTED";
+export const FETCH_COUPONS_FULFILLED = FETCH_COUPONS + "_FULFILLED";
 
 
-export const CouponsTypeEnum = Object.freeze({LOADED : "LOADED", ALL : "ALL"})
+export const CouponsTypeEnum = Object.freeze({LOADED: "LOADED", ALL: "ALL"});
 
 
 const initialState = {
-    couponsType : CouponsTypeEnum.ALL,
-    toBeFetched: false,
-    filters: {},
-    search: {},
-    arr: [],
+	couponsType: CouponsTypeEnum.ALL,
+	toBeFetched: false,
+	filters: {},
+	search: {},
+	arr: [],
 
-    array_filter :[]
-}
+	array_filter: []
+};
 
 // to be moved to config
 const MINIMUM_SEARCH_LENGTH = 3;
 
 const SearchSortFilterReducer = (state = initialState, action) => {
 
-    switch (action.type) {
+	switch (action.type) {
+		case RESET :
+			return {
+				...initialState
+			};
 
-        case SET_SORT :
-            return {
-                ...state,
-                sort: action.payload,
-                toBeFetched: true
-            }
+		case SET_SORT :
+			return {
+				...state,
+				sort: action.payload,
+				toBeFetched: true
+			};
 
-        case SET_FILTERS :
-            return {
-                ...state,
-                filters: action.payload,
-                toBeFetched: true
-            }
+		case SET_FILTERS :
+			return {
+				...state,
+				filters: action.payload,
+				toBeFetched: true
+			};
 
-        case SET_SEARCH  :
-            return {
-                ...state,
-                search: action.payload,
-                toBeFetched: !!action.payload.searchString && action.payload.searchString.length >= MINIMUM_SEARCH_LENGTH
-            }
+		case SET_SEARCH  :
+			return {
+				...state,
+				search: action.payload,
+				toBeFetched: !!action.payload.searchString && action.payload.searchString.length >= MINIMUM_SEARCH_LENGTH
+			};
 
-        case FETCH_COUPONS_PENDING :
-            return {
-                ...state,
-                toBeFetched: false,
-                isLoading: false
+		case FETCH_COUPONS_PENDING :
+			return {
+				...state,
+				toBeFetched: false,
+				isLoading: false
 
-            }
+			};
 
-        case FETCH_COUPONS_REJECTED :
-            return {
-                ...state,
-                toBeFetched: false,
-                isLoading: false
+		case FETCH_COUPONS_REJECTED :
+			return {
+				...state,
+				toBeFetched: false,
+				isLoading: false
 
-            }
+			};
 
-        case FETCH_COUPONS_FULFILLED : {
-            console.log(action.payload.data.response)
-            return {
-                ...state,
-                toBeFetched: false,
-                isLoading: false,
-                arr: action.payload.data.response
+		case FETCH_COUPONS_FULFILLED : {
+			console.log(action.payload.data.response);
+			return {
+				...state,
+				toBeFetched: false,
+				isLoading: false,
+				arr: action.payload.data.response
 
-            };
-        }
+			};
+		}
 
-        default             :
-            return {...state}
-    }
-}
+		default             :
+			return {...state};
+	}
+};
 
-export default SearchSortFilterReducer
+export default SearchSortFilterReducer;
