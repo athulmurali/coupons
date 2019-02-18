2d08e2423ef25382fde9e80843a9b68ae82ac6b1
Feroz Shaik <feroz.shaik@us.royalahold.net>
2019-02-18 18:20:54 -0500

    no message

--- src/redux/reducers/DisplayCouponReducer.js
+++ src/redux/reducers/DisplayCouponReducer.js
@@ -19,34 +19,34 @@ const DisplayCouponsReducer = (state = initialState, action) => {
 
 	switch (action.type) {
 
-		case  UPDATE_COUPON_DETAILS :
-			return {
-				...state, ...action.payload
-			};
+	case  UPDATE_COUPON_DETAILS :
+		return {
+			...state, ...action.payload
+		};
 
-		case UPDATE_DISPLAY_COUPON_STATE :
-			return {
-				...state, ...action.payload
-			};
+	case UPDATE_DISPLAY_COUPON_STATE :
+		return {
+			...state, ...action.payload
+		};
 
-		case RESET :
-			return {
-				...initialState
-			};
+	case RESET :
+		return {
+			...initialState
+		};
 
 
-		case FETCH_COUPONS_FULFILLED : {
-			return {
-				...state,
-				toBeFetched: false,
-				isLoading: false,
-				allCoupons: action.payload.data.response
+	case FETCH_COUPONS_FULFILLED : {
+		return {
+			...state,
+			toBeFetched: false,
+			isLoading: false,
+			allCoupons: action.payload.data.response
 
-			}
+		};
 
-		}
-		default :
-			return state;
+	}
+	default :
+		return state;
 
 	}
 
