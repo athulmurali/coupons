74b087bf24223ab864bcd997c6884d17cb01cac1
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-02-19 18:32:02 -0500

    removed unwanted files in redux

--- src/redux/reducers/UserIdentification.js
+++ src/redux/reducers/UserIdentification.js
@@ -1,25 +1,2 @@
 export const UPDATE_COUPON_DETAILS  = "UPDATE_COUPON_DETAILS";
 export const RESET = "RESET";
-
-
-const initialState = {
-	couponDetails : [],
-	searchedCoupons :[],
-	couponDetailsSearchedCopy :[],
-	count: 0,
-	LoadedCouponsTrigger: false,
-};
-
-const UserIdentification = (state = initialState, action)=>{
-	switch (action.type) {
-	case "UPDATE_COUPON_DETAILS" :  return {
-		...state,...action.payload
-	};
-	case RESET : return {
-		...initialState
-	};
-	default : return state;
-	}
-};
-
-export default  UserIdentification;
