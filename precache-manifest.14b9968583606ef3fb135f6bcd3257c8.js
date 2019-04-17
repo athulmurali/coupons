3975283a37f9fc9a781930eded18fcfca1b63852
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-04-17 08:50:29 -0400

    Updates

---
+++ precache-manifest.14b9968583606ef3fb135f6bcd3257c8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec68b4fc6f991cc5ec9603a519cc0b9b",
+    "url": "/coupons/static/media/addNew.ec68b4fc.svg"
+  },
+  {
+    "revision": "afeedd3eb4cf14a4edc8",
+    "url": "/coupons/static/css/main.5694f019.chunk.css"
+  },
+  {
+    "revision": "a2b853898fe6f980580279c24f58c455",
+    "url": "/coupons/static/media/success.a2b85389.svg"
+  },
+  {
+    "revision": "174e0afcff89b46684c6",
+    "url": "/coupons/static/js/1.174e0afc.chunk.js"
+  },
+  {
+    "revision": "3f9c7aed09e75009d8f0",
+    "url": "/coupons/static/js/runtime~main.3f9c7aed.js"
+  },
+  {
+    "revision": "dc06e95b340840f992ac02905bbb7cd5",
+    "url": "/coupons/static/media/new-filter-arrow-down.dc06e95b.svg"
+  },
+  {
+    "revision": "fd74b81394d6d07fb348e95555c5298a",
+    "url": "/coupons/static/media/icon-phone-white.fd74b813.svg"
+  },
+  {
+    "revision": "fce8444ce717f92ff364b6b1e97dbed1",
+    "url": "/coupons/static/media/icon-card-gray.fce8444c.svg"
+  },
+  {
+    "revision": "662c45accf8de989e7c1334d38c2ab00",
+    "url": "/coupons/static/media/new-filter-arrow-up.662c45ac.svg"
+  },
+  {
+    "revision": "b46f1a83a1ad8d69ecee1d6d7ba54273",
+    "url": "/coupons/static/media/Close-X-2.b46f1a83.svg"
+  },
+  {
+    "revision": "afeedd3eb4cf14a4edc8",
+    "url": "/coupons/static/js/main.afeedd3e.chunk.js"
+  },
+  {
+    "revision": "8a437d714c4c41d8b528cd442c442173",
+    "url": "/coupons/static/media/addedNew.8a437d71.svg"
+  },
+  {
+    "revision": "331bedf4f04d9d4543085d96e05e5251",
+    "url": "/coupons/static/media/coupons-attract-Images-03.331bedf4.png"
+  },
+  {
+    "revision": "a3fd470ffed6ce3cef06fc5ac98281a8",
+    "url": "/coupons/static/media/coupons-attract-Images-04.a3fd470f.png"
+  },
+  {
+    "revision": "78c7ab4937b40f7a002fb2b25c4c65c9",
+    "url": "/coupons/static/media/coupons-attract-Images-05.78c7ab49.png"
+  },
+  {
+    "revision": "d79a6d9d261e7ceb2fa827568e195ef6",
+    "url": "/coupons/static/media/no-account-npImage.d79a6d9d.png"
+  },
+  {
+    "revision": "377886eef4b07a9b6218c5855ac39b9f",
+    "url": "/coupons/static/media/icon-card-white.377886ee.svg"
+  },
+  {
+    "revision": "6b3c16ef9bedb5a4e1263c21dd21498e",
+    "url": "/coupons/static/media/icon-phone-gray.6b3c16ef.svg"
+  },
+  {
+    "revision": "174e0afcff89b46684c6",
+    "url": "/coupons/static/css/1.8fb083fa.chunk.css"
+  },
+  {
+    "revision": "344ff933bea1a4315cd604e21fce28df",
+    "url": "/coupons/index.html"
+  }
+];
