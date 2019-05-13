7489d7238e77bc9ce3b93b01c6fdf4775350470d
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-05-13 17:10:02 -0400

    #COUP-77 Updated changes to be done - Added todo notes in DisplayCoupons.jsx

--- src/components/DisplayCouponComponent/DisplayCoupons.jsx
+++ src/components/DisplayCouponComponent/DisplayCoupons.jsx
@@ -1,3 +1,9 @@
+// Todo : Confirm bubble layout designs with UX designer
+// Todo : Confirm about the location of gas savings
+// Todo : Too many apis to manage if gas rewards is a an api and total savings is another
+// Todo : The position of more savings and savings dashboard is to be changed after discussion
+//  CheckConfirm for possible backend api
+
 import React from "react";
 import Header from "../HeaderComponent/Header";
 import "./DisplayCoupons.css";
