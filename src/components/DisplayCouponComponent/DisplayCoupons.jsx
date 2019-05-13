ad2862c99e107d6ba684937f4672b78651747177
Athul Muralidharan <athul.muralidharan@retailbusinessservices.com>
2019-05-13 16:31:23 -0400

    #COUP-77 Refactored code - Moved react styles for divs in ExpiringCoupons component to a constant

--- src/components/DisplayCouponComponent/DisplayCoupons.jsx
+++ src/components/DisplayCouponComponent/DisplayCoupons.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../HeaderComponent/Header";
 import "./DisplayCoupons.css";
-import {connect} from "react-redux";
 import {ROUTE_HOME_PAGE} from "../../utils/RouteConstants";
 import {fetchCategories, updateLoaded} from "../../redux/actions/SearchSortFilter";
 import WelcomeHeader from "../WelcomeHeader";
@@ -13,57 +12,58 @@ import {RESTART_TIMER_EVENTS} from "../../config/config";
 import CouponCardsWithSearch from "../CouponCardComponent/CouponCardsWithSearch";
 
 import ExpiringCoupons from "../ExpiringCouponsComponent/ExpiringCoupons";
+import connect from "react-redux/es/connect/connect";
+
 class Coupons extends React.Component {
 
 	componentDidMount() {
 		console.log("Mounted!");
-		const {props} =this;
+		const {props} = this;
 		RESTART_TIMER_EVENTS.map(event => window.addEventListener(event, this.props.startTimer));
 		props.startTimer();
 
-		if(!props.userInfo){
+		if (!props.userInfo) {
 			props.history.push(ROUTE_HOME_PAGE);
-		}
-
-		else if (!props.isTimedOut)
-		{
+		} else if (!props.isTimedOut) {
 			props.fetchCategories();
-			props.updateLoaded({loaded :false});
+			props.updateLoaded({loaded: false});
 		}
 
 	}
+
 	componentWillReceiveProps(nextProps, nextContext) {
 
-		const {props} =this;
+		const {props} = this;
 
-		if(!nextProps.userInfo || !!nextProps.isTimedOut ){
+		if (!nextProps.userInfo || !!nextProps.isTimedOut) {
 			props.history.push(ROUTE_HOME_PAGE);
 		}
 	}
+
 	componentWillUnmount() {
 		console.log("Unmounted!");
 		RESTART_TIMER_EVENTS.map(event => window.removeEventListener(event, this.startTimer));
 	}
 
 	render() {
-		const {props} =this;
+		const {props} = this;
 		console.log("rendering DisplayCoupons.jsx");
 		return props.userInfo && <div className="pointerEventsNone">
 			<WelcomeHeader userName={props.userInfo.firstName}/>
 			<Header/>
-			<ExpiringCoupons gasRewards={"$0.02"} totalSavings={"$366.12"} userName={userName} />
+			<ExpiringCoupons gasRewards={"$0.02"} totalSavings={"$366.12"} userName={props.userInfo.firstName}/>
 			{/*<PrintComponent hideLoadedCoupons={this.state.hideLoadedCoupons}*/}
-							{/*componentRef={this.componentRef}></PrintComponent>*/}
+			{/*componentRef={this.componentRef}></PrintComponent>*/}
 
 			<div className="displayCouponsContainer">
-					 <SideBar />
-					 <CouponCardsWithSearch/>
-					 <LogOutSuccessPopUp/>
-					 <LogOutPromptPopup/>
-				</div>
+				<SideBar/>
+				<CouponCardsWithSearch/>
+				<LogOutSuccessPopUp/>
+				<LogOutPromptPopup/>
+			</div>
 		</div>
+	}
 }
-
 const mapStateToProps = (state) => {
 	return {
 		userInfo: state.DisplayCouponsReducer.userInfo,
--- src/components/ExpiringCouponsComponent/ExpiringCoupons.jsx
+++ src/components/ExpiringCouponsComponent/ExpiringCoupons.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import "./ExpiringCoupons.css";
+import PropTypes from "prop-types";
+
 const ExpiringCouponsDiv = styled.div`
 	display: flex;
 	justify-content: center;
@@ -10,46 +12,42 @@ const ExpiringCouponsDiv = styled.div`
 	color: black;
 	
 `;
+const styles = {
+	totalSavingsContainer: {borderRight: "3px solid black"},
+	totalSavingsText: {fontWeight: "bold", marginTop: "7px"},
+};
 
 class ExpiringCoupons extends React.Component {
-	
-	render() {
-		return(
-			<ExpiringCouponsDiv>
-				
-				<div class="talk-bubble tri-right border round btm-left-in" style={{width: "400px"}}>
-  			<div class="talktext">
-						<div >
-							{this.props.userName}, start saving with digital coupons.
-						
-						</div>
 
-  			</div>
+	render() {
+		return (
+			<ExpiringCouponsDiv>
+				<div class="talk-bubble tri-right border round btm-left-in" style={{width: "400px"}}>
+					<div class="talktext">
+						<div>
+							{this.props.userName}, start saving with digital coupons.
+						</div>
+					</div>
 				</div>
 				<div class="saving_div">
-					<div class="total_savings" style={{borderRight: "3px solid black"}}>
-				Your total Savings.
-						<p style={{
-							fontWeight: "bold",
-							marginTop: "7px"}}>{this.props.totalSavings}</p>
-				
+					<div class="total_savings" style={styles.totalSavingsContainer}>
+						Your total Savings.
+						<p style={styles.totalSavingsText}>{this.props.totalSavings}</p>
 					</div>
-					
 					<div class="total_savings">
-					Your total Gas Rewards.
-						<p style={{
-							fontWeight: "bold",
-							marginTop: "7px"}}>{this.props.gasRewards}</p>
+						Your total Gas Rewards.
+						<p style={styles.totalSavingsText}>{this.props.gasRewards}</p>
 					</div>
-				
-					
-				
 				</div>
-			
 			</ExpiringCouponsDiv>
 		);
 	}
 
 }
+
+ExpiringCoupons.propTypes = {
+	totalSavings: PropTypes.string.isRequired,
+	gasRewards: PropTypes.string.isRequired,
+};
 
 export default ExpiringCoupons;
